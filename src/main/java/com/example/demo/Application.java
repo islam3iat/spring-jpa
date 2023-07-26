5dd80c77d7ad58d2913bd2787ba423c51980f2f4
islam3iat <72038531+islam3iat@users.noreply.github.com>
2023-07-26 10:35:04 +0100

    add

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +17,31 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+@Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            PageRequest pageRequest=PageRequest.of(0,20,Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
+
+
+        };
+
+    }
+
+    private static void sorting(StudentRepository studentRepository) {
+        Sort sort = Sort.by("firstName").ascending().and(Sort.by("age").ascending());
+        studentRepository.findAll(sort).forEach(System.out::println);
+    }
+
+    private static void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker =new Faker();
+        for (int i=0;i<20;i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email=String.format("%s.%s@start.edu",firstName,lastName);
+            Student student = new Student(firstName, lastName, email, faker.number().numberBetween(17, 55));
+            studentRepository.save(student);
+        }
+    }
 }
